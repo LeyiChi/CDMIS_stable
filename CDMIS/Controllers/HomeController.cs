031192d944fd646d598e9bbc26b1221e24fe0905
csq8899 <sqchi1991@163.com>
2016-11-09 18:41:55 +0800

    recommit

---
+++ CDMIS/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CDMIS.ServiceReference;
+using CDMIS.Models;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Drawing.Imaging;
+using CDMIS.CommonLibrary;
+
+namespace CDMIS.Controllers
+{
+    [StatisticsTracker]
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
